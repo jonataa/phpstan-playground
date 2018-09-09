c05baadf54de0c4a783010305c51402e585fd2e4
Jonata Weber <jonataa@gmail.com>
2018-09-09 20:00:21 -0300

    Fixed bug-01

--- app/HelloWorld.php
+++ app/HelloWorld.php
@@ -5,7 +5,7 @@ namespace App;
 class HelloWorld
 {
   
-  public function sayHello(\DateTimeImutable $date)
+  public function sayHello(\DateTimeImmutable $date)
   {
     echo 'Hello, ' . $date->format('j. n. Y');
   }
