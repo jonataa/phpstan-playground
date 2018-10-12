cc38f76fa1bdb7d0884934af78171d861d100ff2
Jonata Weber <jonataa@gmail.com>
2018-10-12 10:40:11 -0300

    update base constructor

--- app/Base.php
+++ app/Base.php
@@ -8,8 +8,12 @@ class Base
     /** @var int */
     protected $number;
 
+    /** @var int */
+    protected $double;
+
     public function __construct(int $number)
     {
         $this->number = $number;
+        $this->double = $number * 2;
     }    
 }
